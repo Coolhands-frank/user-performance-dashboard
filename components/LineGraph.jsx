ee82d84695d4ca73297742bd9e10d51a34a8f07d
Elendu Franklin <elendufranklin66@gmail.com>
2025-03-21 11:39:46 +0100

    bug fix

--- components/LineGraph.jsx
+++ components/LineGraph.jsx
@@ -26,7 +26,7 @@ export default function LineGraph() {
   return (
     <div className="w-full h-64 p-4">
       <ResponsiveContainer width="100%" height="100%">
-        <LineChart data={data} key={percentile}>
+        <LineChart data={data}>
           {/* X-Axis */}
           <XAxis
             dataKey="percentile"
